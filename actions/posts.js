82f8bcf6ae1ce73bde9064749a9ceb12454f29cf
jiangjanny <109901097+jiangjanny@users.noreply.github.com>
2024-06-25 23:12:52 +0800

    Add files via upload

---
+++ actions/posts.js
@@ -0,0 +1,48 @@
+"use server"
+import { storePost } from '@/lib/posts'
+import { redirect } from 'next/navigation'
+import { uploadImage } from '@/lib/cloudinary'
+export async function createPost(prevState, formData) {
+    const title = formData.get('title')
+    const image = formData.get('image')
+    const content = formData.get('content')
+
+    let errors = []
+
+    if (!title || title.trim().length === 0) {
+        errors.push("Title is rwquired.")
+    }
+
+    if (!content || content.trim().length === 0) {
+        errors.push("Content is required.")
+    }
+
+    if (!image) {
+        errors.push("Image is required.")
+    }
+
+    if (errors.length > 0) {
+        return { errors }
+    }
+
+    let imageUrl;
+    try {
+        imageUrl = await uploadImage(image)
+    } catch (error) {
+        throw new Error('Image upload failed, post was not created. please try again later.')
+    }
+
+    await storePost({
+        imageUrl: imageUrl,
+        title,
+        content,
+        userId: 1,
+    })
+    redirect('/feed')
+
+}
+
+
+export async function togglePostLikeStatus(postId) {
+
+}
---
+++ app/api/posts/route.js
@@ -0,0 +1,3 @@
+export function POST(request) {
+    response.json({ message: 'Hello from the API' })
+}
